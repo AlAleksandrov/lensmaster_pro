d661ad6481b7c4cb56fbe0a25260f7f9bf2cb5e8
AlAleksandrov <algal.bg@gmail.com>
2026-02-23 00:40:08 +0200

    final update

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from inventory.views import EquipmentListView
+from inventory.views import EquipmentListView, EquipmentDetailView
 
 app_name = 'inventory'
 
 urlpatterns = [
     path('', EquipmentListView.as_view(), name='equipment_list'),
+    path('<int:pk>/', EquipmentDetailView.as_view(), name='equipment_detail'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from inventory.models import Equipment
 
 
@@ -9,12 +9,25 @@ class EquipmentListView(ListView):
     context_object_name = 'equipment_items'
 
     def get_queryset(self):
-        return Equipment.objects.filter(is_active=True)
+        qs = Equipment.objects.filter(is_active=True).order_by('equipment_type')
+
+        selected_type = self.request.GET.get('type')
+        if selected_type:
+            qs = qs.filter(equipment_type__iexact=selected_type)
+
+        try:
+            qs = qs.prefetch_related('productions')
+        except AttributeError:
+            pass
+
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         ordered_types = ['Camera', 'Lens', 'Lighting', 'Drone', 'Other']
+
+        context['selected_type'] = self.request.GET.get('type')
 
         grouped = {}
         for item in context['equipment_items']:
@@ -32,5 +45,24 @@ class EquipmentListView(ListView):
         ]
 
         context['grouped_equipment'] = grouped_equipment
+
+        return context
+
+
+class EquipmentDetailView(DetailView):
+    model = Equipment
+    template_name = 'inventory/equipment_detail.html'
+    context_object_name = 'item'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        item = self.object
+
+        if hasattr(item, 'productions'):
+            productions = item.productions.all()
+        else:
+            productions = getattr(item, 'production_set', item.__class__).all() if hasattr(item, '__class__') else []
+
+        context['productions'] = productions
 
         return context
