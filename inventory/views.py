8ecbffc9c56b315308c381816fa1e922e0d9f91a
AlAleksandrov <algal.bg@gmail.com>
2026-03-07 16:39:22 +0200

    Added more functionalities

--- inventory/views.py
+++ inventory/views.py
@@ -7,24 +7,24 @@ class EquipmentListView(ListView):
     model = Equipment
     template_name = 'inventory/equipment_list.html'
     context_object_name = 'equipment_list'
-    paginate_by = 4
+    paginate_by = 6
 
     def get_queryset(self):
-        return Equipment.objects.filter(is_active=True).prefetch_related('productions')
+        return Equipment.objects.filter(is_active=True).prefetch_related('productions').order_by('equipment_type', 'brand', 'model')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        qs = self.get_queryset()
+        page_items = context['page_obj']
 
         types = Equipment.EquipmentType.choices
 
         grouped = []
         for val, label in types:
-            items = qs.filter(equipment_type=val)
-            if items.exists():
+            items_in_group = [item for item in page_items if item.equipment_type == val]
+            if items_in_group:
                 grouped.append({
                     'type': label,
-                    'items': items,
+                    'items': items_in_group,
                 })
 
         context['grouped_equipment'] = grouped
